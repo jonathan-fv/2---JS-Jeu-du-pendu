e15520667f92348f0f510cda550105a1d39b2780
jonathan-fv <jonathan.fenoud-viard@outlook.fr>
2021-11-07 18:12:15 +0100

    add js code

---
+++ sources/js/main.js
@@ -0,0 +1,123 @@
+const wordE1 = document.getElementById('word')
+const wrongLettersE1 = document.getElementById('wrong-letters')
+const playAgainBtn = document.getElementById('play-button')
+const popup = document.getElementById('popup-container')
+const notification = document.getElementById('notification-container')
+const finalMessage = document.getElementById('final-message')
+
+const figureParts = document.querySelectorAll('.figure-part')
+
+const words = [
+    'application',
+    'programme',
+    'ordinateur',
+    'tablette',
+    'perceuse',
+    'moustache',
+    'cuniculiculture',
+    'acrimonie',
+    'surnuméraire',
+    'Thaumaturge'
+]
+let selectedWord = words[Math.floor(Math.random() * words.length)]
+const correctLetters = []
+const wrongLetters = []
+
+//Show hidden word
+function displayWord(){
+    wordE1.innerHTML = `
+    ${selectedWord
+    .split('')
+    .map(
+        letter => `
+        <span class="letter">
+        ${correctLetters.includes(letter) ? letter : ''}
+        </span>
+        `
+    )
+    .join('')}
+    `
+    const innerWord = wordE1.innerText.replace(/\n/g, '')
+
+    if(innerWord === selectedWord){
+        finalMessage.innerText = 'Félicitation! Vous avez gagné!'
+        popup.style.display = 'flex'
+    }
+}
+
+//Update the wrong letters
+function updateWrogLetterE1(){
+    //Display wrong letter
+    wrongLettersE1.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Mauvaise(s) lettre(s)</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `
+
+    //Display parts
+    figureParts.forEach((part, index) => {
+        const errors = wrongLetters.length
+
+        if(index < errors){
+            part.style.display = 'block'
+        }
+        else{
+            part.style.display = "none"
+        }
+    })
+
+    //Check if lost
+    if(wrongLetters.length === figureParts.length){
+        finalMessage.innerText = 'Désolé! Vous avez perdu!'
+        popup.style.display = 'flex'
+    }
+}
+
+//Show notification
+function showNotification(){
+    notification.classList.add('show')
+
+    setTimeout(() =>{
+        notification.classList.remove('show')
+    }, 2000)
+}
+
+//Keydown letter press
+window.addEventListener('keydown', e =>{
+    if(e.keyCode >= 65 && e.keyCode <= 90){
+        const letter = e.key
+        if(selectedWord.includes(letter)){
+            if(!correctLetters.includes(letter)){
+                correctLetters.push(letter)
+                displayWord()
+            }
+            else{
+                showNotification()
+            }
+        }
+        else{
+            if(!wrongLetters.includes(letter)){
+                wrongLetters.push(letter)
+                updateWrogLetterE1()
+            }
+            else{
+                showNotification()
+            }
+        }
+    }
+})
+
+//Restart game and play again
+playAgainBtn.addEventListener('click', () =>{
+    //Empty arrays
+    correctLetters.splice(0)
+    wrongLetters.splice(0)
+
+    selectedWord = words[Math.floor(Math.random() * words.length)]
+    displayWord()
+
+    updateWrogLetterE1()
+
+    popup.style.display = 'none'
+})
+
+displayWord()
